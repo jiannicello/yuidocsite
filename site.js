9548de8eb5bf8c5188b844a4ac16ac9d9ac9f2e3
John Iannicello <johnicello@gmail.com>
2013-06-28 00:21:17 -0400

    DataSearch incorporated into site.js

--- site.js
+++ site.js
@@ -3,7 +3,9 @@
 var fs = require('fs'),
     http = require('http'),
     nconf = require('nconf'),
-    UrlHelper = require('./lib/urlhelper').UrlHelper;
+    UrlHelper = require('./lib/urlhelper').UrlHelper,
+    DataSearcher = require('./lib/datasearcher').DataSearcher,
+    searcher = null;
 
 
 function writeErrorResponse(res, err) {
@@ -25,34 +27,39 @@ nconf.argv();
 console.log('nconf.get port: ' + nconf.get('port'));
 console.log('nconf.get search_desc: ' + nconf.get('search_desc'));
 
-var port = Number(nconf.get('port'));
-    
-http.createServer(function (req, res) {
-    var urlhelper = new UrlHelper(req),
-        readFile = function (fullpath, callback) {
-            fs.readFile(fullpath, function (err, data) {
-                if (err) {
-                    writeErrorResponse(res, err);
-                } else {
-                    callback(data);
-                }
+searcher = new DataSearcher({search_desc: nconf.get('search_desc')});
+searcher.load(function () {
+    var port = Number(nconf.get('port'));
+
+    http.createServer(function (req, res) {
+        var urlhelper = new UrlHelper(req),
+            readFile = function (fullpath, callback) {
+                fs.readFile(fullpath, function (err, data) {
+                    if (err) {
+                        writeErrorResponse(res, err);
+                    } else {
+                        callback(data);
+                    }
+                });
+            };
+
+        console.log('---------------------');
+        console.log('urlhelper.urlstr: ' + urlhelper.urlstr);
+        console.log('urlhelper.searchTerm: ' + urlhelper.searchTerm);
+
+        if (urlhelper.urlstr === '/') {
+            writeResponseRedirect(res, '/docs/index.html');
+        } else if (urlhelper.searchTerm) {
+            writeResponse(res, searcher.search(urlhelper.searchTerm));
+        } else if (urlhelper.ext === '.html') {
+            readFile(urlhelper.fullpath, function (data) {
+                writeResponse(res, data.toString());
             });
-        };
-    
-    console.log('---------------------');
-    console.log('urlhelper.urlstr: ' + urlhelper.urlstr);
-    console.log('urlhelper.ext: ' + urlhelper.ext);
-    
-    if (urlhelper.urlstr === '/') {
-        writeResponseRedirect(res, '/docs/index.html');
-    } else if (urlhelper.ext === '.html') {
-        readFile(urlhelper.fullpath, function (data) {
-            writeResponse(res, data.toString());
-        });
-    } else {
-        readFile(urlhelper.fullpath, function (data) {
-            writeResponse(res, data);
-        });
-    }
-    
-}).listen(port);
+        } else {
+            readFile(urlhelper.fullpath, function (data) {
+                writeResponse(res, data);
+            });
+        }
+
+    }).listen(port);
+}); //end searcher.load
